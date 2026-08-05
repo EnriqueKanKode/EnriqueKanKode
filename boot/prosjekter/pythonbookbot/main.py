346f03db78513ed41b2f37b0b0e2d1deacca6ee0
Enrique Roman Heras Cano <e.roman.h.c@gmail.com>
2026-08-05 21:38:45 +0200

    det var en gammel bookbot i mappen prosjekter. Den nye bookbot er i kurser mappen under kurs nummer 3. Building Bookbot

--- boot/prosjekter/pythonbookbot/main.py
+++
@@ -1,9 +0,0 @@
-
-
-def get_book_tekst(filepath : path) --> str:
-    with open(filepath) as f:
-        file_content = f.read
-        print(file_content)
-        
-get_book_tekst("books /frankeinstein.txt")
-
