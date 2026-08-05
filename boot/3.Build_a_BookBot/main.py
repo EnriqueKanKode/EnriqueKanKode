8d09770348714935aa839dae9f1c63a5638413fa
Enrique Roman Heras Cano <e.roman.h.c@gmail.com>
2026-08-05 22:28:54 +0200

    ble ferdig med kurs 3.Bookbot og begynner på kurs 4.Learn_Git

--- boot/3.Build_a_BookBot/main.py
+++ boot/3.Build_a_BookBot/main.py
@@ -1,13 +1,31 @@
+import sys
 from stats import word_count, get_book_text, character_count, chars_dict_to_sorted_list
 
-def pirnt_report(book_path: str, word_count: )
+def print_report(book_path: str, word_count: int, sorted_list: list[(str, int)]) -> None:
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print(f"--------- Character Count -------")
+    for _tuple in sorted_list:
+        if _tuple[0].isalpha():
+            print(f"{_tuple[0]}: {_tuple[1]}")
+    print("============= END ===============")
+    
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) == 2:
+        pass
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     book_content = get_book_text(path)
+    print_report(path, word_count(book_content),chars_dict_to_sorted_list(character_count(book_content)))
     
-    print(f"Found {word_count(book_content)} total words")
-    print(chars_dict_to_sorted_list(character_count(book_content)))
+    
+    #print(f"Found {word_count(book_content)} total words")
+    #print(chars_dict_to_sorted_list(character_count(book_content)))
 
 
     
